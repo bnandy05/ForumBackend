69ce852b17585f038cf905b8cb318464247f3e9d
bandiiskola <98553047+bnandy05@users.noreply.github.com>
2025-01-27 12:51:17 +0100

    Route-ok
    Hiányzó route-ok: Jelszóváltoztatás, Profil, stb.

--- ForumBackend/routes/web.php
+++ ForumBackend/routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::controller(TopicsController::class)->group(function () {
+        Route::get('/', 'index')->name('forum.fooldal');
+        
+        Route::get('/feltoltes', 'createTopic')->name('forum.feltoltes');
+        Route::post('/feltoltes', 'createTopic')->name('forum.feltoltes.post');
+        
+        Route::get('/topik/{id}', 'show')->name('forum.topik.show');
+        
+        Route::post('/topik/{id}/komment', 'addComment')->name('forum.komment.hozzaadas');
+        
+        Route::post('/topik/{id}/vote', 'voteTopic')->name('forum.topik.vote');
+        
+        Route::post('/komment/{id}/vote', 'voteComment')->name('forum.komment.vote');
+
+        Route::delete('/topik/{id}', 'deleteTopic')->name('forum.topik.torles');
+
+        Route::delete('/komment/{id}', 'deleteComment')->name('forum.komment.torles');
+
+        Route::middleware(['auth', 'admin'])->group(function () {
+            Route::delete('/topik/admin/{id}', 'deleteAdminTopic')->name('forum.topik.torles');
+            Route::delete('/komment/admin/{id}', 'deleteAdminComment')->name('forum.komment.torles');
+        });   
+    });
 });
