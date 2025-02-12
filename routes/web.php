273d41e647a040c341893e10c6d359b662ce40ce
bnandy05 <98553047+bnandy05@users.noreply.github.com>
2025-02-12 16:46:59 +0100

    Működő api végpontok + Angular

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return file_get_contents(public_path('index.html'));
-})->where('any', '^(?!api).*$');
+Route::get('frontend/{any?}', function () {
+    return file_get_contents(public_path('frontend/index.html'));
+})->where('any', '.*');
 
